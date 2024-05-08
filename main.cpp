271993e898ec511d3a257d41aa45466f31f75781
D1ksan <ekhestt@yandex.ru>
2024-05-08 18:53:00 +0300

    feat: add tests for sphere and cylinder, update maincpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
 #include "./tests/square_test.cpp"
 #include "./tests/circle_test.cpp"
 #include "./tests/triangle_test.cpp"
+#include "./tests/cylinder_test.cpp"
+#include "./tests/sphere_test.cpp"
